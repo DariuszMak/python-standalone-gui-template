07a6489c83ad266ac238395825b38934a0393cee
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2023-06-29 09:21:10 +0200

    add basic main_window form

---
+++ app/ui/auto_generated/replace_generated.bat
@@ -0,0 +1,3 @@
+pyside6-uic.exe --from-imports --output .\app\ui\auto_generated\main_window.py .\app\ui\forms\main_window.ui
+
+pyside6-rcc.exe --output .\app\ui\auto_generated\files_rc.py .\app\ui\forms\files.qrc
