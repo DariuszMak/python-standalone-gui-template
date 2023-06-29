c311247085a394a045f17ea695c85241edb10d83
darMak <darekmakarewiczpraca@gmail.com>
2023-06-29 10:37:41 +0200

    Basic qss loader

--- app/application.py
+++ app/application.py
@@ -1,10 +1,39 @@
 # -*- coding: utf-8 -*-
 """Application module."""
 import sys
+import os
 
 
 from PySide6.QtWidgets import QApplication, QMainWindow
 from app.ui.auto_generated.main_window import Ui_MainWindow
+
+
+class StylesLoader:
+    """Loads style from files."""
+
+    @staticmethod
+    def resource_path(relative_path):
+        """Gets absolute path of project and concatenate with provided path."""
+        base_path = os.path.abspath(".")
+
+        return os.path.realpath(os.path.join(base_path, relative_path))
+
+    @staticmethod
+    def get_qss_from_file():
+        """Loads qss theme from file."""
+        theme_file_path = StylesLoader.resource_path("app/ui/themes/main_theme.qss")
+
+        with open(theme_file_path, "r") as f:
+            style = f.read()
+
+        return style
+
+    @staticmethod
+    def setup_stylesheets(window):
+        """Sets new stylesheet to provided window."""
+        stylesheet_from_file = StylesLoader.get_qss_from_file()
+
+        window.setStyleSheet(stylesheet_from_file)
 
 
 class MainWindow(QMainWindow):
@@ -14,6 +43,7 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        StylesLoader.setup_stylesheets(self)
 
 
 def run():
