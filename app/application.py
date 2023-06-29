efc73c0ef489119eb10282817285dde017bc1301
darMak <darekmakarewiczpraca@gmail.com>
2023-06-29 09:37:11 +0200

    add working example

--- app/application.py
+++ app/application.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 """Application module."""
 import sys
-from PySide6.QtWidgets import QApplication, QWidget
+
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+from app.ui.auto_generated.main_window import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    """MainWindow class."""
+
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
 
 def run():
     """Main function that runs application."""
     app = QApplication(sys.argv)
 
-    window = QWidget()
+    window = MainWindow()
     window.show()
 
-    app.exec_()
+    sys.exit(app.exec())
 
 
 def hello_world():
