ffa26ea65ce5f6630738e5703edc43e4c1872f08
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2023-07-01 21:04:42 +0200

    better mypy

--- test/helpers/test_style_loader.py
+++ test/helpers/test_style_loader.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """Style loader class test module."""
+import pytest  # type: ignore
+
 
 from app.helpers.style_loader import StyleLoader
 
@@ -7,3 +9,6 @@ from app.helpers.style_loader import StyleLoader
 def test_get_qss_from_file() -> None:
     """Getting real path test."""
     assert isinstance(StyleLoader.get_qss_from_file(), str)
+
+    with pytest.raises(Exception):
+        StyleLoader.get_qss_from_file("")
