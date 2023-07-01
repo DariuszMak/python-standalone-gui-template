c6526c2a512a1aa7a608422eb9260ace4dbf51e2
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2023-07-01 16:06:48 +0200

    minor change

--- test/helpers/test_style_loader.py
+++ test/helpers/test_style_loader.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """Style loader class test module."""
-import os
 
 from app.helpers.style_loader import StyleLoader
 
 
 def test_get_qss_from_file() -> None:
     """Getting real path test."""
-    assert isinstance(StyleLoader.get_qss_from_file(os.path.join("app", "ui", "themes", "main_theme.qss")), str)
+    assert isinstance(StyleLoader.get_qss_from_file(), str)
