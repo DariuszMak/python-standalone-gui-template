019f4fb8cfc26a43d9f50083d02a43ab2e41880f
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2023-07-01 21:14:42 +0200

    split unit tests

--- test/helpers/test_style_loader.py
+++ test/helpers/test_style_loader.py
@@ -7,8 +7,11 @@ from app.helpers.style_loader import StyleLoader
 
 
 def test_get_qss_from_file() -> None:
-    """Getting real path test."""
+    """Loading style from file."""
     assert isinstance(StyleLoader.get_qss_from_file(), str)
 
+
+def test_get_qss_from_file_with_incorrect_path() -> None:
+    """When loading style from file has incorrect path."""
     with pytest.raises(Exception):
         StyleLoader.get_qss_from_file("")
