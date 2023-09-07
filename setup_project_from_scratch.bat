90bfd7c79dd7fe6ffe4651ab4bef78ab8bb87f74
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2023-09-07 10:01:27 +0200

    setup_project_from_scratch.bat

--- setup_project_from_scratch.bat
+++ setup_project_from_scratch.bat
@@ -1,7 +1,23 @@
-docker system df ; docker stop $(docker ps -a -q) ; docker rm $(docker ps -a -q) ; docker system prune -a ; docker system df
+@echo off
+@REM git fetch
+@REM git reset --hard
+@REM git clean -x -d -f
 
-git fetch
-git reset --hard
-git clean -x -d -f
+rmdir /s /q .\venv
+
+python -m pip install --upgrade pip
+python -m pip install virtualenv
+python -m virtualenv venv
+
+@REM .\venv\Scripts\activate.bat
+venv\Scripts\Activate.ps1
+
+python -m pip install -r requirements.txt
+
+docker system df
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker system prune -a
+docker system df
 
 docker-compose run app
