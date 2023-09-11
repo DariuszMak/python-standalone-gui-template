a824e86357609f0f58a17582d13d08db6c34ba91
darMak <darekmakarewiczpraca@gmail.com>
2023-09-11 11:11:46 +0200

    split scratch documents generators

--- setup_local_project_from_scratch.bat
+++ setup_local_project_from_scratch.bat
@@ -1,12 +1,6 @@
 @echo off
 rmdir /s /q .\venv
 
-docker system df
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker system prune -a
-docker system df
-
 python -m pip install --upgrade pip
 python -m pip install virtualenv
 python -m virtualenv venv
@@ -15,6 +9,3 @@ python -m virtualenv venv
 venv\Scripts\Activate.ps1
 
 python -m pip install -r requirements_dev.txt
-
-docker-compose build
-docker-compose run app
