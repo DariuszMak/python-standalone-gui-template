74696d1254a4a231bc669fafced7e35250acdd2c
Dariusz Makarewicz <dariusz.makarewicz@gigaclear.com>
2025-07-04 12:22:21 +0200

    cleanup

--- src/main.py
+++ src/main.py
@@ -1,15 +1,7 @@
 """Main module."""
 
-import os
-import sys
-
-from src import application, gui_setup
 
 TRUE_ENV_VARIABLES_VALUES = "true", "1", "t"
 
 if __name__ == "__main__":
-    if not (getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS")):
-        gui_setup.create_mocs()
-
-    if os.getenv("DOCKER_RUNTIME", "False").lower() not in TRUE_ENV_VARIABLES_VALUES:
-        application.run()
+    print("Hello World!")
