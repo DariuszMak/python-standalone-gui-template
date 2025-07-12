73c1e2e5db5c785e38f1e934b24e9bd271e7307b
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2025-07-12 11:10:46 +0200

    move api runner

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import sys
 import threading
 
 from src import application, gui_setup
-from src.api_runner import run_api
+from src.api.api_runner import run_api
 from src.helpers.setup_logging import setup_logging
 
 TRUE_ENV_VARIABLES_VALUES = "true", "1", "t"
