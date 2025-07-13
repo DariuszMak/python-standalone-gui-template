cff20de9f477bbbe7d6252d0ec4f2565b15f061e
Dariusz.Makarewicz <Dariusz.Makarewicz@gigaclear.com>
2025-07-13 16:09:15 +0200

    fix import error

--- src/ui/warning_dialog.py
+++ src/ui/warning_dialog.py
@@ -2,7 +2,7 @@ from PySide6 import QtCore, QtGui
 from PySide6.QtCore import QEvent, Qt
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QDialog, QWidget
-from PyQt6.QtGui import QMouseEvent
+from PySide6.QtGui import QMouseEvent
 
 
 from src.helpers.style_loader import StyleLoader
