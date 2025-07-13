9f67c84e4d1583c0b7dc5bdd38681eb9feb16df1
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2025-07-13 09:08:05 +0200

    add swagger UI

--- src/api/app.py
+++ src/api/app.py
@@ -1,5 +1,8 @@
 from litestar import Litestar
+from litestar.openapi.config import OpenAPIConfig
 
 from src.api.routes import ping
 
-app = Litestar(route_handlers=[ping])
+openapi_config = OpenAPIConfig(title="My API", version="0.1.0", description="API documentation for my service")
+
+app = Litestar(route_handlers=[ping], openapi_config=openapi_config)
