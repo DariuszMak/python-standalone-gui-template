c1979142167dec4d62c2027fe3a15488126b588d
Dariusz.Makarewicz <Dariusz.Makarewicz@gigaclear.com>
2025-07-13 15:48:26 +0200

    fix io_file.py

--- src/helpers/io_file.py
+++ src/helpers/io_file.py
@@ -5,18 +5,10 @@ import sys
 class IOFile:
     @staticmethod
     def get_real_path_from_relative_path(relative_path: str) -> str:
-        base_path = None
-
-        try:
-            base_path = sys._MEIPASS
-        except Exception:
-            base_path = os.path.abspath(".")
-
+        base_path = getattr(sys, "_MEIPASS", os.path.abspath("."))
         return os.path.realpath(os.path.join(base_path, relative_path))
 
     @staticmethod
     def load_file_content(path: str) -> str:
         with open(IOFile.get_real_path_from_relative_path(path)) as f:
-            content = f.read()
-
-        return content
+            return f.read()
