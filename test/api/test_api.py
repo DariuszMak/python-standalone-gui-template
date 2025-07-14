73096e9d6c2f08f5d3038c6257d1ae03eb27c5dc
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2025-07-14 17:10:19 +0200

    add swagger test

--- test/api/test_api.py
+++ test/api/test_api.py
@@ -8,3 +8,10 @@ def test_ping_route():
         response = client.get("/ping")
         assert response.status_code == 200
         assert response.json() == {"message": "pong"}
+
+
+def test_swagger_ui_available():
+    with TestClient(app) as client:
+        resp = client.get("/schema/swagger")
+        assert resp.status_code == 200
+        assert "html" in resp.headers["content-type"]
