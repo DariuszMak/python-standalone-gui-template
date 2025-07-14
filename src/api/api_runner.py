d201752c749b96b071bcabd21df0b7a1474de73b
Dariusz Makarewicz <dariusz.makarewicz@gigaclear.com>
2025-07-14 17:23:40 +0200

    remove api_runner

--- src/api/api_runner.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-
-from src.api.app import app
-
-
-def run_api() -> None:
-    config = uvicorn.Config(app, host="127.0.0.1", port=8000, log_level="info")
-    server = uvicorn.Server(config)
-    server.run()
--- src/api/app.py
+++ src/api/app.py
@@ -1,3 +1,4 @@
+import uvicorn
 from litestar import Litestar
 from litestar.openapi.config import OpenAPIConfig
 
@@ -5,4 +6,11 @@ from src.api.routes import ping
 
 openapi_config = OpenAPIConfig(title="My API", version="0.1.0", description="API documentation for my service")
 
+
 app = Litestar(route_handlers=[ping], openapi_config=openapi_config)
+
+
+def run_api() -> None:
+    config = uvicorn.Config(app, host="127.0.0.1", port=8000, log_level="info")
+    server = uvicorn.Server(config)
+    server.run()
