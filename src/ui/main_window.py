f8a0a8b5ac793d488bfe86ff98f9bd58814e2cc3
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2025-09-07 09:34:01 +0200

    add fade in and out effect

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -1,8 +1,6 @@
-import logging
-
-from PySide6.QtCore import QEvent
+from PySide6.QtCore import QEvent, QPropertyAnimation, QEasingCurve
 from PySide6.QtGui import QCloseEvent
-
+import logging
 from src.helpers.style_loader import StyleLoader
 from src.ui.draggable_main_window import DraggableMainWindow
 from src.ui.forms.moc_main_window import Ui_MainWindow
@@ -10,12 +8,12 @@ from src.ui.warning_dialog import WarningDialog
 
 logger = logging.getLogger(__name__)
 
-
 class MainWindow(DraggableMainWindow):
     def __init__(self) -> None:
         super().__init__()
 
         self.setMinimumSize(500, 400)
+        self._is_closing = False  
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
@@ -27,6 +25,28 @@ class MainWindow(DraggableMainWindow):
         self.ui.btn_minimize.clicked.connect(self.showMinimized)
         self.ui.btn_maximize_restore.clicked.connect(self.toggle_maximize_restore)
         self.ui.btn_close.clicked.connect(self.close)
+
+        self.fade_in_animation()
+
+    def fade_in_animation(self):
+        self.setWindowOpacity(0.0)
+        self.anim = QPropertyAnimation(self, b"windowOpacity")
+        self.anim.setDuration(600)
+        self.anim.setStartValue(0.0)
+        self.anim.setEndValue(1.0)
+        self.anim.setEasingCurve(QEasingCurve.Type.InOutQuad)
+        self.anim.start()
+
+    def fade_out_animation(self):
+        self.anim = QPropertyAnimation(self, b"windowOpacity")
+        self.anim.setDuration(600)
+        self.anim.setStartValue(1.0)
+        self.anim.setEndValue(0.0)
+        self.anim.setEasingCurve(QEasingCurve.Type.InOutQuad)
+
+        
+        self.anim.finished.connect(self._final_close)
+        self.anim.start()
 
     def show_warning_dialog(self):
         dlg = WarningDialog(self)
@@ -45,11 +65,22 @@ class MainWindow(DraggableMainWindow):
             self.showMaximized()
         self._is_maximized = not self._is_maximized
 
-    def changeEvent(self, event: QEvent) -> None:  # noqa: N802
+    def changeEvent(self, event: QEvent) -> None:  
         if event.type() == QEvent.Type.LanguageChange:
             self.ui.retranslateUi(self)
         super().changeEvent(event)
 
-    def closeEvent(self, event: QCloseEvent) -> None:  # noqa: N802
+    def _final_close(self):
+        self._is_closing = True
+        super().close()  
+
+    def closeEvent(self, event: QCloseEvent) -> None:
         logger.info("Closing main window...")
-        super().closeEvent(event)
+
+        if self._is_closing:
+            
+            super().closeEvent(event)
+        else:
+            
+            event.ignore()
+            self.fade_out_animation()
