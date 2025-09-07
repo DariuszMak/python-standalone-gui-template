39d38bfabdd4da4b8b1dbdeeaf40dbe2a24c5e54
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2025-09-07 09:56:38 +0200

    run opacity only with support

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -7,6 +7,7 @@ from src.helpers.style_loader import StyleLoader
 from src.ui.draggable_main_window import DraggableMainWindow
 from src.ui.forms.moc_main_window import Ui_MainWindow
 from src.ui.warning_dialog import WarningDialog
+from PySide6.QtGui import QGuiApplication
 
 logger = logging.getLogger(__name__)
 
@@ -14,6 +15,8 @@ logger = logging.getLogger(__name__)
 class MainWindow(DraggableMainWindow):
     def __init__(self) -> None:
         super().__init__()
+
+        self._supports_opacity = QGuiApplication.platformName().lower() not in ["wayland", "xcb"]
 
         self.setMinimumSize(500, 400)
         self._is_closing = False
@@ -28,10 +31,12 @@ class MainWindow(DraggableMainWindow):
         self.ui.btn_minimize.clicked.connect(self.showMinimized)
         self.ui.btn_maximize_restore.clicked.connect(self.toggle_maximize_restore)
         self.ui.btn_close.clicked.connect(self.close)
-
-        self.fade_in_animation()
+        if self._supports_opacity:
+                self.fade_in_animation()
 
     def fade_in_animation(self):
+        if not self._supports_opacity:
+            return
         self.setWindowOpacity(0.0)
         self.anim = QPropertyAnimation(self, b"windowOpacity")
         self.anim.setDuration(600)
@@ -41,6 +46,9 @@ class MainWindow(DraggableMainWindow):
         self.anim.start()
 
     def fade_out_animation(self):
+        if not self._supports_opacity:
+            self._final_close()
+            return
         self.anim = QPropertyAnimation(self, b"windowOpacity")
         self.anim.setDuration(600)
         self.anim.setStartValue(1.0)
