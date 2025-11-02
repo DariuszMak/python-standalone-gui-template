eb6f16d0ddd09a9efa61d0a474c634a8227a4408
Dariusz Makarewicz <dariusz.makarewicz@gigaclear.com>
2025-11-02 12:32:29 +0100

    make strict linting

--- scripts/format_and_lint.ps1
+++ scripts/format_and_lint.ps1
@@ -6,6 +6,8 @@ uv run ruff check --fix --select I test src --exclude 'moc_.*\.py|files_rc\.py'
 uv run pip-audit
 uv run ruff check test src --exclude 'moc_.*\.py|files_rc\.py'
 uv run ruff format --check test src --exclude 'moc_.*\.py|files_rc\.py'
-uv run mypy --explicit-package-bases test src --exclude 'moc_.*\.py|files_rc\.py'
+uv run mypy --strict test\ src\ --exclude 'moc_.*\.py|files_rc\.py' ; 
+
+# uv run mypy --explicit-package-bases test src --exclude 'moc_.*\.py|files_rc\.py'
 # uv run mypy --explicit-package-bases --check-untyped-defs test src --exclude 'moc_.*\.py|files_rc\.py'
 # uv run mypy --strict test src
