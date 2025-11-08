d04064db6fc1ef2147a95b3c2bd9ba2b5d0e5d9b
Dariusz.Makarewicz <Dariusz.Makarewicz@gigaclear.com>
2025-11-08 17:29:01 +0100

    create helpers.py

--- src/ui/clock_widget/clock_widget.py
+++ src/ui/clock_widget/clock_widget.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import math
-from datetime import UTC, datetime, time, timedelta
+from datetime import UTC, datetime
 
 from PySide6.QtCore import QPointF, QTimer
 from PySide6.QtGui import QColor, QFont, QPainter, QPaintEvent, QPen
@@ -9,28 +9,8 @@ from PySide6.QtWidgets import QWidget
 
 from src.ui.clock_widget.clock_pid import ClockPID
 from src.ui.clock_widget.data_types import ClockHands, HandsPosition
+from src.ui.clock_widget.helpers import calculate_clock_hands_angles, polar_to_cartesian
 from src.ui.clock_widget.pid import PID
-
-
-def polar_to_cartesian(center: QPointF, length: float, angle_radians: float) -> QPointF:
-    x = center.x() + math.sin(angle_radians) * length
-    y = center.y() - math.cos(angle_radians) * length
-    return QPointF(x, y)
-
-
-def calculate_clock_hands_angles(start_dt: datetime, duration: timedelta) -> ClockHands:
-    midnight = datetime.combine(start_dt.date(), time(0, 0, 0), tzinfo=start_dt.tzinfo)
-    start_ms = int((start_dt - midnight).total_seconds() * 1000)
-    elapsed_ms = int(duration.total_seconds() * 1000)
-
-    start_s = start_ms / 1000.0
-    elapsed_s = elapsed_ms / 1000.0
-
-    seconds_angle = start_s % 60.0 + elapsed_s
-    minutes_angle = (start_s / 60.0) % 60.0 + elapsed_s / 60.0
-    hours_angle = (start_s / 3600.0) % 12.0 + elapsed_s / 3600.0
-
-    return ClockHands(second=float(seconds_angle), minute=float(minutes_angle), hour=float(hours_angle))
 
 
 class ClockWidget(QWidget):
