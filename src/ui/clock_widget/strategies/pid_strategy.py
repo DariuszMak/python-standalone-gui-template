59c01506f793c82bca2c5469c565bab432d3b5ad
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2025-11-18 13:16:39 +0100

    add pid strategy file

---
+++ src/ui/clock_widget/strategies/pid_strategy.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from src.ui.clock_widget.pid import PID
+from src.ui.clock_widget.strategies.movement_strategy import MovementStrategy
+
+
+class PIDMovementStrategy(MovementStrategy):
+    def __init__(self, kp: float, ki: float, kd: float) -> None:
+        self.pid = PID(kp=kp, ki=ki, kd=kd)
+
+    def update(self, current_value: float, target_value: float) -> float:
+        error = target_value - current_value
+        return current_value + self.pid.update(error)
+
+    def reset(self) -> None:
+        self.pid.reset()
