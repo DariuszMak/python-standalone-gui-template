9ec170e966f70f57b92dfb6e53342afa78048273
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2025-11-18 13:23:06 +0100

    autoformat

--- src/ui/clock_widget/strategies/tick_strategy.py
+++ src/ui/clock_widget/strategies/tick_strategy.py
@@ -4,6 +4,5 @@ from src.ui.clock_widget.strategies.movement_strategy import MovementStrategy
 
 
 class TickMovementStrategy(MovementStrategy):
-
     def update(self, current_value: float, target_value: float) -> float:
         return float(target_value)
