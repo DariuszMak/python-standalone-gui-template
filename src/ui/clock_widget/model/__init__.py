3ff4b03d9906146105b40c4e484cf81819493078
Dariusz.Makarewicz <Dariusz.Makarewicz@gigaclear.com>
2025-11-28 15:17:30 +0100

    autoformat

--- src/ui/clock_widget/model/__init__.py
+++ src/ui/clock_widget/model/__init__.py
@@ -1,14 +1,14 @@
 from .clock_pid import ClockPID
 from .data_types import ClockHands, HandsPosition
-from .helpers import calculate_clock_hands_angles, polar_to_cartesian, format_datetime
+from .helpers import calculate_clock_hands_angles, format_datetime, polar_to_cartesian
 from .pid import PID
 
 __all__ = [
-    "ClockPID",
+    "PID",
     "ClockHands",
+    "ClockPID",
     "HandsPosition",
     "calculate_clock_hands_angles",
-    "polar_to_cartesian",
     "format_datetime",
-    "PID",
+    "polar_to_cartesian",
 ]
