7d5d49d71ddfbfd615b777f4db5907467ef1934b
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2025-12-18 13:04:33 +0100

    add current_time endpoint

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,6 +1,14 @@
+
+from datetime import datetime,  UTC
+
 from litestar import get
 
 
 @get("/ping")
 async def ping() -> dict[str, str]:
     return {"message": "pong"}
+
+
+@get("/time")
+async def current_time() -> dict[str, str]:
+    return {"datetime": datetime.now(UTC).isoformat()}
