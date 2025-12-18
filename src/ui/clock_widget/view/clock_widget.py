54a93d3093469829ecf4f065ee2d2d519fb2beea
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2025-12-18 16:12:08 +0100

    current time refactor

--- src/ui/clock_widget/view/clock_widget.py
+++ src/ui/clock_widget/view/clock_widget.py
@@ -7,7 +7,6 @@ from PySide6.QtGui import QPainter, QPaintEvent
 from PySide6.QtWidgets import QWidget
 
 from src.ui.clock_widget.controller.clock_controller import ClockController
-from src.ui.clock_widget.model.clock_angles import ClockAngles
 from src.ui.clock_widget.view.helpers import convert_clock_pid_to_cartesian
 from src.ui.clock_widget.view.painter import paint_clock_face, paint_current_time, paint_hands
 from src.ui.clock_widget.view.tick_events import TickEventSubject
@@ -24,15 +23,17 @@ class ClockWidget(QWidget):
         self._timer.timeout.connect(self._tick_subject.notify)
         self._timer.start(15)
 
-        self.controller = ClockController(datetime.now(UTC))
+        self.controller = ClockController(self.get_current_time())
 
     def on_tick(self) -> None:
-        self.current_time = datetime.now(UTC).astimezone()
-        self.controller.update(self.current_time)
+        self.controller.update(self.get_current_time())
         self.update()
 
+    def get_current_time(self) -> datetime:
+        return datetime.now(UTC).astimezone()
+
     def reset(self) -> None:
-        self.controller.reset(datetime.now(UTC).astimezone())
+        self.controller.reset(self.get_current_time())
         self.update()
 
     def paintEvent(self, event: QPaintEvent) -> None:  # noqa: N802, ARG002
@@ -40,14 +41,7 @@ class ClockWidget(QWidget):
         painter.setRenderHint(QPainter.RenderHint.Antialiasing)
 
         center, radius, font_size = paint_clock_face(painter, self.rect, self.palette)
-        self.controller.clock_angles = ClockAngles(
-            self.controller.clock_angles.clock_hands_angles.second,
-            self.controller.clock_angles.clock_hands_angles.minute,
-            self.controller.clock_angles.clock_hands_angles.hour,
-        )
-
         hands_position = convert_clock_pid_to_cartesian(self.controller.clock_angles, center, radius)
 
         paint_hands(painter, center, hands_position)
-
-        paint_current_time(self.current_time, painter, center, radius, font_size)
+        paint_current_time(self.get_current_time(), painter, center, radius, font_size)
