ae8a32e023cf0441c4aa96c04f539f25fb435216
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2025-12-18 13:15:38 +0100

    add route

--- src/api/app.py
+++ src/api/app.py
@@ -4,11 +4,11 @@ import uvicorn
 from litestar import Litestar
 from litestar.openapi.config import OpenAPIConfig
 
-from src.api.routes import ping
+from src.api.routes import current_time, ping
 
 openapi_config = OpenAPIConfig(title="My API", version="0.1.0", description="API documentation for my service")
 
-app = Litestar(route_handlers=[ping], openapi_config=openapi_config)
+app = Litestar(route_handlers=[ping, current_time], openapi_config=openapi_config)
 
 
 def run_api() -> None:
