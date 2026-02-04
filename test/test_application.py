621af85c665ca152a1c7e46032d7298be432edd6
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-02-04 17:58:53 +0100

    fix qtbot

--- test/test_application.py
+++ test/test_application.py
@@ -5,7 +5,7 @@ from PySide6.QtWidgets import QApplication
 from src.application import create_app
 
 
-def test_create_app_wires_everything(_qtbot):
+def test_create_app_wires_everything(qtbot):
     app = QApplication.instance()
     assert app is not None
 
