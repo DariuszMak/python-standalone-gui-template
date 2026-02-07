f6daa31434ed8845c851d9c244929823f914fe6d
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-02-07 16:43:26 +0100

    change panel priority

--- src/config/config.py
+++ src/config/config.py
@@ -5,20 +5,20 @@ from src.config.env import EnvLoaderMixin
 
 @dataclass(frozen=True)
 class Config(EnvLoaderMixin):
-    api_host: str = "127.0.0.1"
-    api_port: int = 8001
     panel_host: str = "127.0.0.1"
     panel_port: int = 8000
+    api_host: str = "127.0.0.1"
+    api_port: int = 8001
     react_host: str = "127.0.0.1"
     react_port: int = 8005
 
     @property
-    def api_base_url(self) -> str:
-        return f"http://{self.api_host}:{self.api_port}"
-
-    @property
     def panel_api_base_url(self) -> str:
         return f"http://{self.panel_host}:{self.panel_port}"
+
+    @property
+    def api_base_url(self) -> str:
+        return f"http://{self.api_host}:{self.api_port}"
 
     @property
     def react_base_url(self) -> str:
