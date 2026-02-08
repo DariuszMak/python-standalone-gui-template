a8283f6319739b4fce0f95e30012b3547292120d
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-02-08 19:19:33 +0100

    update host

--- src/config/config.py
+++ src/config/config.py
@@ -5,21 +5,19 @@ from src.config.env import EnvLoaderMixin
 
 @dataclass(frozen=True)
 class Config(EnvLoaderMixin):
-    panel_host: str = "127.0.0.1"
+    host: str = "127.0.0.1"
     panel_port: int = 8001
-    api_host: str = "127.0.0.1"
     api_port: int = 8000
-    react_host: str = "127.0.0.1"
     react_port: int = 8002
 
     @property
     def panel_api_base_url(self) -> str:
-        return f"http://{self.panel_host}:{self.panel_port}"
+        return f"http://{self.host}:{self.panel_port}"
 
     @property
     def api_base_url(self) -> str:
-        return f"http://{self.api_host}:{self.api_port}"
+        return f"http://{self.host}:{self.api_port}"
 
     @property
     def react_base_url(self) -> str:
-        return f"http://{self.react_host}:{self.react_port}"
+        return f"http://{self.host}:{self.react_port}"
