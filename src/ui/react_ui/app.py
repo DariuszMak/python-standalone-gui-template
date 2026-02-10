4b3dd7c8db6c624e4b50ae34d3ef4abfed835c2c
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-02-10 18:57:30 +0100

    revert som app.py changes

--- src/ui/react_ui/app.py
+++ src/ui/react_ui/app.py
@@ -1,36 +1,27 @@
 import os
-
 import uvicorn
+from pathlib import Path
 from litestar import Litestar
 from litestar.static_files import StaticFilesConfig
 
-from src import STATIC_CATALGUE_NAME
-from src.ui.react_ui.routes import now
-
 
 def create_app() -> Litestar:
+    dist_dir = Path(__file__).parent / "dist"
+
     return Litestar(
-        route_handlers=[
-            now,
-        ],
         static_files_config=[
             StaticFilesConfig(
                 path="/",
-                directories=[os.path.join(os.path.dirname(__file__), STATIC_CATALGUE_NAME)],
+                directories=[dist_dir],
                 html_mode=True,
             )
-        ],
+        ]
     )
 
 
 def run() -> None:
-    uvicorn.run(
-        create_app(),
-        host=os.getenv("REACT_HOST", "127.0.0.1"),
-        port=int(os.getenv("REACT_PORT", 8002)),
-        log_level="info",
-    )
 
+    host = os.getenv("REACT_HOST", "127.0.0.1")
+    port = int(os.getenv("REACT_PORT", "8000"))
 
-if __name__ == "__main__":
-    run()
+    uvicorn.run(create_app(), host=host, port=port, log_level="info")
