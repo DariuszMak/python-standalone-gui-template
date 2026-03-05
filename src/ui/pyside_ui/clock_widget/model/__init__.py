239eeb7976bd4244f38a8d78443b5a0b808f5270
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-03-05 13:22:14 +0100

    fix import

--- src/ui/pyside_ui/clock_widget/model/__init__.py
+++ src/ui/pyside_ui/clock_widget/model/__init__.py
@@ -1,11 +1,9 @@
 from ..view.helpers import calculate_clock_hands_angles, format_datetime, polar_to_cartesian
-from .clock_angles import ClockAngles
 from .data_types import ClockHands, HandsPosition
 from .pid import PID
 
 __all__ = [
     "PID",
-    "ClockAngles",
     "ClockHands",
     "HandsPosition",
     "calculate_clock_hands_angles",
