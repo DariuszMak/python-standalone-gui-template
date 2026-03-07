ab094c20970f6a476f39687f110192e912e5feac
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-03-07 15:54:12 +0100

    split test

--- test/ui/pyside_ui/test_tray_manager.py
+++ test/ui/pyside_ui/test_tray_manager.py
@@ -10,8 +10,6 @@ from pytestqt.qtbot import QtBot
 
 from src.ui.pyside_ui.dialog_windows.main_window import MainWindow
 
-
-
 def test_minimize_hides_window_to_tray(qtbot: QtBot, monkeypatch: MonkeyPatch) -> None:
     window = MainWindow()
     qtbot.addWidget(window)
