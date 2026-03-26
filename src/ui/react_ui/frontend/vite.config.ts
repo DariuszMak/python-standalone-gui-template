9d8f0b4898215d652c65ba162b481c7e81bac387
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-03-26 08:55:53 +0100

    further configuration

--- src/ui/react_ui/frontend/vite.config.ts
+++ src/ui/react_ui/frontend/vite.config.ts
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+  },
   server: {
-    proxy: {
-      "/time": "http://localhost:8000",
-    },
+    proxy: { "/time": "http://localhost:8000" },
   },
 });
