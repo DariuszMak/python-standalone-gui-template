f6e503e6c69fdcb27dd3d524a951b2d676d29a4d
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-03-28 20:11:53 +0100

    modify vulture command

--- scripts/format_and_lint.ps1
+++ scripts/format_and_lint.ps1
@@ -3,7 +3,7 @@ uv run ruff check --fix tests src --exclude 'moc_.*\.py|files_rc\.py'
 uv run ruff check --fix --unsafe-fixes tests src --exclude 'moc_.*\.py|files_rc\.py'
 uv run ruff check --fix --select I tests src --exclude 'moc_.*\.py|files_rc\.py'
 
-uv run vulture src tests --exclude "moc_.*\.py,files_rc\.py" --min-confidence 80
+uv run vulture src tests --exclude "src/ui/pyside_ui/forms" --min-confidence 80
 
 uv run pip-audit
 uv run ruff check tests src --exclude 'moc_.*\.py|files_rc\.py'
