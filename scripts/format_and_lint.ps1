f0cece4d45e09d849eca600c44096ac85bea46ad
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-03-28 20:22:31 +0100

    change sequence

--- scripts/format_and_lint.ps1
+++ scripts/format_and_lint.ps1
@@ -3,11 +3,12 @@ uv run ruff check --fix tests src --exclude 'moc_.*\.py|files_rc\.py'
 uv run ruff check --fix --unsafe-fixes tests src --exclude 'moc_.*\.py|files_rc\.py'
 uv run ruff check --fix --select I tests src --exclude 'moc_.*\.py|files_rc\.py'
 
-uv run vulture src tests --exclude "src/ui/pyside_ui/forms" --min-confidence 80
 
 uv run pip-audit
 uv run ruff check tests src --exclude 'moc_.*\.py|files_rc\.py'
 uv run ruff format --check tests src --exclude 'moc_.*\.py|files_rc\.py'
+
+uv run vulture src tests --exclude "src/ui/pyside_ui/forms" --min-confidence 80
 
 uv run mypy --strict tests src --exclude 'moc_.*\.py|files_rc\.py'
 
