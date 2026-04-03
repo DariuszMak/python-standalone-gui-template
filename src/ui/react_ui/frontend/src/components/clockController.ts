8ab0e25c9707a0b6068d09b53a8f9c2ace70c537
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-04-03 17:43:45 +0200

    add one bracket

--- src/ui/react_ui/frontend/src/components/clockController.ts
+++ src/ui/react_ui/frontend/src/components/clockController.ts
@@ -28,8 +28,8 @@ export class ClockController {
       second: this._clockHands.second + ss.update(getShortestInterval(this._clockHands.second, target.second, 60)),
       minute: this._clockHands.minute + sm.update(getShortestInterval(this._clockHands.minute, target.minute, 60)),
       hour: this._clockHands.hour + sh.update(getShortestInterval(this._clockHands.hour, target.hour, 12)),
-    };
-
+  };
+  }
   reset(newStartTime: Date = new Date(0)): void {
     this._startTime = newStartTime;
     this._clockHands = { second: 0, minute: 0, hour: 0 };
