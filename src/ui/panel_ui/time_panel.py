48039b2f6a446ed3206d3fc067440f5eba090eff
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-04-08 17:35:09 +0200

    autoformat

--- src/ui/panel_ui/time_panel.py
+++ src/ui/panel_ui/time_panel.py
@@ -187,9 +187,7 @@ def create_layout() -> pn.Column:
 
     time_display: pn.pane.Markdown = pn.pane.Markdown("No data", sizing_mode="stretch_width")  # type: ignore
 
-    button: pn.widgets.Button = pn.widgets.Button(
-        name="Fetch time from API", button_type="primary"
-    )
+    button: pn.widgets.Button = pn.widgets.Button(name="Fetch time from API", button_type="primary")
 
     async def _fetch() -> None:
         try:
