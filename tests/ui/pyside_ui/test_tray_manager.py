6f0d7f5706f3b064e407fbe8939c5ef4716fc90f
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-05-06 16:40:18 +0200

    minor change

--- tests/ui/pyside_ui/test_tray_manager.py
+++ tests/ui/pyside_ui/test_tray_manager.py
@@ -6,7 +6,6 @@ from src.ui.pyside_ui.dialog_windows.main_window import MainWindow
 
 
 def test_minimize_hides_window_to_tray(qtbot: QtBot, monkeypatch: pytest.MonkeyPatch) -> None:
-
     window = MainWindow(fetch_server_time=False)
     qtbot.addWidget(window)
     window.show()
