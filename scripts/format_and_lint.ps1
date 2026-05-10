b52c606ff63679bc9ee47c4b7183ddd82c6f7612
Dariusz Makarewicz <darekmakarewicz1994@gmail.com>
2026-05-10 19:28:15 +0200

    change sequence of folders

--- scripts/format_and_lint.ps1
+++ scripts/format_and_lint.ps1
@@ -1,23 +1,23 @@
-uv run ruff format tests src --exclude 'moc_.*\.py|files_rc\.py'
-uv run ruff check --fix tests src --exclude 'moc_.*\.py|files_rc\.py'
-uv run ruff check --fix --unsafe-fixes tests src --exclude 'moc_.*\.py|files_rc\.py'
-uv run ruff check --fix --select I tests src --exclude 'moc_.*\.py|files_rc\.py'
+uv run ruff format src tests --exclude 'moc_.*\.py|files_rc\.py'
+uv run ruff check --fix src tests --exclude 'moc_.*\.py|files_rc\.py'
+uv run ruff check --fix --unsafe-fixes src tests --exclude 'moc_.*\.py|files_rc\.py'
+uv run ruff check --fix --select I src tests --exclude 'moc_.*\.py|files_rc\.py'
 
 
 uv run pip-audit
-uv run ruff check tests src --exclude 'moc_.*\.py|files_rc\.py'
-uv run ruff format --check tests src --exclude 'moc_.*\.py|files_rc\.py'
+uv run ruff check src tests --exclude 'moc_.*\.py|files_rc\.py'
+uv run ruff format --check src tests --exclude 'moc_.*\.py|files_rc\.py'
 
-uv run ruff check tests src --output-format json > ruff-report.json
+uv run ruff check src tests --output-format json > ruff-report.json
 
 
 uv run vulture src tests --exclude "src/ui/pyside_ui/forms" --min-confidence 80
 
 
-uv run mypy --strict tests src --exclude 'moc_.*\.py|files_rc\.py'
+uv run mypy --strict src tests --exclude 'moc_.*\.py|files_rc\.py'
 
-# uv run mypy --explicit-package-bases tests src --exclude 'moc_.*\.py|files_rc\.py'
-# uv run mypy --explicit-package-bases --check-untyped-defs tests src --exclude 'moc_.*\.py|files_rc\.py'
-# uv run mypy --strict tests src
+# uv run mypy --explicit-package-bases src tests --exclude 'moc_.*\.py|files_rc\.py'
+# uv run mypy --explicit-package-bases --check-untyped-defs src tests --exclude 'moc_.*\.py|files_rc\.py'
+# uv run mypy --strict src tests
 
 uv run lint-imports --config pyproject.toml
